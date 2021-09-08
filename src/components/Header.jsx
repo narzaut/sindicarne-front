934a0e6a58440dca9e65fda2c1f521ec485bbfef
narz <i.arzaut@itecriocuarto.org.ar>
2021-09-07 21:45:48 -0300

    v1.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function Header() {
   return (
-		<div className='bg-header-blue py-2 w-full flex items-center justify-center '>
-			<img className='w-3/4 md:w-2/4 lg:w-1/5' src={process.env.PUBLIC_URL + '/assets/logo.png'} />
+		<div className='bg-header-blue py-4 w-full flex items-center justify-center '>
+			<img className='w-2/3 md:w-2/4 lg:w-1/5' src={process.env.PUBLIC_URL + '/assets/logo.png'} />
 		</div>
     
   );
